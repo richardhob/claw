371333d052968fdbab5b30f46d6f1f46a995ac10
Ricky <richardhob@gmail.com>
2025-07-16 16:35:03 -0700

    First failing test

--- scripts/set_voltage.py
+++ scripts/set_voltage.py
@@ -1,4 +1,46 @@
 
+import sys
 import ctypes
 import dwfconstants 
 
+# CLI Application which can be used to set the output voltage on either (or
+# both) Analog output channels
+#
+# This is basically for debugging.
+
+def get_dll():
+    ''' Get the DLL based on the system (copied from DWF example) '''
+    if sys.platform.startswith("win"):
+        dwf = cdll.dwf
+    elif sys.platform.startswith("darwin"):
+        dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+    else:
+        dwf = cdll.LoadLibrary("libdwf.so")
+
+    return dwf
+
+def get_device(dll, num=-1):
+    ''' Use the DLL to get the device instance
+    '''
+    pass
+
+def set_voltage(dll, device, channel, voltage):
+    ''' Set the output voltage on the provided channel 
+
+    Args:
+         dll: dll (from get_dll)
+         device: device (from get_device)
+         channel: 
+         voltage: 
+    '''
+    pass
+
+def close(dll, device):
+    ''' Properly shut down the device. '''
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- scripts/test/test_set_voltage.py
+++ scripts/test/test_set_voltage.py
@@ -1,3 +1,18 @@
 
 import ctypes
+import unittest.mock as mock
+
+import pytest
+
 import dwfconstants
+
+# Script under test
+import set_voltage
+
+@pytest.fixture(scope='module')
+def mock_dll():
+    return mock.MagicMock()
+
+def test_get_device(mock_dll):
+    set_voltage.get_device(mock_dll, "device_id")
+    mock_dll.FDwfDeviceOpen.assert_called_once_with("device_id", ctypes.c_int(0))
